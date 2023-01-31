3481152c8574a0ccd5e3b2ec1f18772635338062
hemanthr3 <hemanth@kodnest.com>
2023-01-31 11:54:17 +0530

    conditionals added

--- pages/search.js
+++ pages/search.js
@@ -11,9 +11,9 @@ const Search = ({searchResult}) => {
   const router = useRouter();
   const { location, startDate, endDate, numberOfGuests } = router.query;
 
-  const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
-  const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
-  const range = `${formattedStartDate} - ${formattedEndDate}`;
+  // const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
+  // const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
+  // const range = `${formattedStartDate} - ${formattedEndDate}`;
 
   const coordinate = ()=> {
     return(
@@ -33,9 +33,9 @@ console.log("Search location is",coordinate)
       />
       <main className="flex">
         <section className="flex-grow pt-14 px-6">
-          <p className="text-xs">
+          {/* <p className="text-xs">
             300+ stays - {range && range} - for {numberOfGuests && numberOfGuests} guests
-          </p>
+          </p> */}
           <h1 className="text-3xl font-semibold mb-6">Stays in {location && location}</h1>
           <div className="hidden lg:inline-flex mb-5 gap-3 text-gray-800 whitespace-nowrap">
             <p className="px-4 py-2 border rounded-full cursor-pointer hover:shadow-lg active:scale-95 active:bg-gray-100 transition transform duration-100 ease-out">
